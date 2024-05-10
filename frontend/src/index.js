cc649d750a552526d08ad912f8b5acd057b99b28
Towa Quimbayo <towacurtis@hotmail.com>
2024-05-10 14:54:11 -0700

    installed react for frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+);
