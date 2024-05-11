a95f1dd8b4f36306f04e76d23d262c727dbc675e
Towa Quimbayo <towacurtis@hotmail.com>
2024-05-11 14:52:34 -0700

    Added pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import "./css/global.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import AlimonyClaim from "./pages/AlimonyClaim";
+import AlimonyCalculator from "./pages/AlimonyCalculator";
+import TransactionHistory from "./pages/TransactionHistory";
 import LandingPage from "./components/LandingPage/LandingPage";
 
 export default function App() {
@@ -12,6 +15,9 @@ export default function App() {
       <Route path="/landingPage" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/alimony-claim" element={<AlimonyClaim />} />
+      <Route path="/alimony-calculator" element={<AlimonyCalculator />} />
+      <Route path="/transaction-history" element={<TransactionHistory />} />
     </Routes>
   );
 }
