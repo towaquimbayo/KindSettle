3cfaf086eadc433b844d405f1859685c1ffad7d6
anithaamarnath <anithaamarnath04@gmail.com>
2024-05-11 15:03:27 -0700

    Added hero Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import "./css/global.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/landingPage" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
---
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HeroSection from '../HeroSection/HeroSection'
+
+const LandingPage = () => {
+  return (
+    <div>
+      <HeroSection/>
+    </div>
+  )
+}
+export default LandingPage;
