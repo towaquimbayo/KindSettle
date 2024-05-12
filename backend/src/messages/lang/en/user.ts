1216aa76a57f8d8fb92c9af5211d4127d2e1731f
noufilsaqib <noufil.s@hotmail.com>
2024-05-11 17:32:53 -0700

    Added backend messages

---
+++ backend/src/messages/lang/en/user.ts
@@ -0,0 +1,74 @@
+const noRouteExists = "No route exists for the provided URL. Please try again.";
+const noAccessTokenFound = "No access token found. Please try again.";
+const accessTokenDenied = "Access token denied. Please try again.";
+const userNotFound = "User not found for the provided email. Please try again.";
+const loginEndpointNotFound = "Login endpoint not found. Please try again.";
+const promptEndpointNotFound = "Prompt endpoint not found. Please try again.";
+const forgotPasswordEndpointNotFound =
+  "Forgot password endpoint not found. Please try again.";
+const resetPasswordEndpointNotFound =
+  "Reset password endpoint not found. Please try again.";
+const fetchUsersEndpointNotFound =
+  "Fetch users endpoint not found. Please try again.";
+const deleteUserEndpointNotFound =
+  "Delete user endpoint not found. Please try again.";
+const editUserEndpointNotFound =
+  "Edit user endpoint not found. Please try again.";
+const serverError = "Internal server error. Please try again.";
+const invalidEmailOrPassword = "Invalid email or password. Please try again.";
+const emailExistsError =
+  "An account with this email already exists. Please try again.";
+const userLoginSuccess = "User logged in successfully.";
+const noOtpCodeFound = "No code found for this email.";
+const invalidOtpCode = "Invalid code. Please try again.";
+const otpValidationSuccess = "OTP verified successfully.";
+const resetPasswordEmailSent = "Reset password email sent successfully.";
+const samePasswordError =
+  "New password cannot be the same as the old password. Please try again.";
+const passwordUpdatedSuccess = "Password updated successfully.";
+const deleteUserSuccess = "User deleted successfully.";
+const userUpdatedSuccess = "User updated successfully.";
+const userApiInfoNotFound = "User API info not found.";
+const noFileFound = "No file uploaded.";
+const noPromptProvided = "No prompt provided.";
+const noUserIdProvided = "No user ID provided.";
+const invalidFileType = "Invalid file type. Please upload a PDF file.";
+const filesizeExceeded =
+  "File size too large. Please upload a file less than 2MB.";
+const llmApiPdfConvertError = "Failed to extract text from PDF.";
+const llmApiModelResponseError =
+  "Failed to get model response based on prompt.";
+
+export const messages = {
+  noRouteExists,
+  noAccessTokenFound,
+  accessTokenDenied,
+  userNotFound,
+  loginEndpointNotFound,
+  promptEndpointNotFound,
+  forgotPasswordEndpointNotFound,
+  resetPasswordEndpointNotFound,
+  fetchUsersEndpointNotFound,
+  deleteUserEndpointNotFound,
+  editUserEndpointNotFound,
+  serverError,
+  invalidEmailOrPassword,
+  emailExistsError,
+  userLoginSuccess,
+  noOtpCodeFound,
+  invalidOtpCode,
+  otpValidationSuccess,
+  resetPasswordEmailSent,
+  samePasswordError,
+  passwordUpdatedSuccess,
+  deleteUserSuccess,
+  userUpdatedSuccess,
+  userApiInfoNotFound,
+  noFileFound,
+  noPromptProvided,
+  noUserIdProvided,
+  invalidFileType,
+  filesizeExceeded,
+  llmApiPdfConvertError,
+  llmApiModelResponseError,
+};
