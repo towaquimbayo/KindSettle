0a9370c1046b9cfa0933dc8a6efaf4385b525d52
Towa Quimbayo <towacurtis@hotmail.com>
2024-05-11 21:52:08 -0700

    created first 3 steps for Alimony Claim Forms

--- frontend/src/pages/MyClaims.js
+++ frontend/src/pages/MyClaims.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import Layout from "../components/Layout";
-import "../css/layout.css";
 
 export default function MyClaims() {
   const navigate = useNavigate();
