1fec8abec7597b3332226624b90156f8557ee04d
noufilsaqib <noufil.s@hotmail.com>
2024-05-11 20:37:10 -0700

    Completed jwt authentication

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,55 +1,63 @@
-import { Schema, model, Document } from 'ottoman';
+import { Schema, model } from "ottoman";
+import { connectDB } from "../db/connect";
 
-interface IUser extends Document {
-    name: string;
-    email: string;
-    password: string;
-    dob?: Date;
-    phone?: string;
-    address?: string;
-    monthlyIncome?: number;
-    spouse?: string;
-    created: Date;
-}
+const start = async () => {
+  try {
+    await connectDB({
+      connectionString: process.env.CB_URI,
+      bucketName: process.env.CB_BUCKET,
+      username: process.env.CB_USERNAME,
+      password: process.env.CB_PASSWORD,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
 
 const UserSchema = new Schema({
-    name: { 
-        type: String, 
-        required: true,
-        min: 3,
-        max: 255
-    },
-    email: { 
-        type: String, 
-        required: true,
-        max: 255
-    },
-    password: { 
-        type: String, 
-        required: true,
-        max: 1024, //store hashes
-        min: 6
-    },
-    dob: { 
-        type: Date 
-    },
-    phone: { 
-        type: String 
-    },
-    address: { 
-        type: String 
-    },
-    monthlyIncome: { 
-        type: Number 
-    },
-    spouse: { 
-        type: String 
-    },
-    created: { 
-        type: Date, 
-        default: Date.now()
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 255,
+  },
+  email: {
+    type: String,
+    required: true,
+    maxLength: 255,
+  },
+  password: {
+    type: String,
+    required: true,
+    maxLength: 1024, //store hashes
+    minLength: 6,
+  },
+  dob: {
+    type: Date,
+  },
+  phone: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  monthlyIncome: {
+    type: Number,
+  },
+  spouse: {
+    type: String,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const User = model<IUser>('User', UserSchema);
+const User = model("User", UserSchema, {
+  collectionName: "users",
+  scopeName: "admin",
+});
+
 export default User;
