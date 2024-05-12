6de80a3206093734bc0479ccb8b20c7eced527f8
anithaamarnath <anithaamarnath04@gmail.com>
2024-05-11 17:49:50 -0700

    Article Section

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../HeroSection/HeroSection'
 import Cards from '../Cards/Cards';
+import ArticleSections from '../ArticleSections/ArticleSections';
 
 const LandingPage = () => {
   return (
     <div>
       <HeroSection />
       <Cards />  
+      <ArticleSections />
     </div>
   )
 }
