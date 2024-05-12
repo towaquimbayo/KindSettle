97d8b7d2771e4d04b409420385ed691397e462c2
noufilsaqib <noufil.s@hotmail.com>
2024-05-11 21:16:33 -0700

    Remove logged in check for home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,19 +1,8 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import Layout from "../components/Layout";
 import LandingPage from "../components/LandingPage/LandingPage";
 
 
 export default function Home() {
-  const navigate = useNavigate();
-
-  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-
-  useEffect(() => {
-    if (!isLoggedIn) navigate("/login");
-  }, [isLoggedIn, navigate]);
-
   return (
     <Layout isLandingPage>
       <LandingPage />
