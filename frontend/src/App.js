31b0b32749dd9edd6d9d17dfb1e3ee60916962d7
noufilsaqib <noufil.s@hotmail.com>
2024-05-11 21:06:54 -0700

    Added landing component to home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,16 @@ import "./css/global.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import LandingPage from "./components/LandingPage/LandingPage";
 import AlimonyClaim from "./pages/AlimonyClaim";
 import AlimonyCalculator from "./pages/AlimonyCalculator";
 import TransactionHistory from "./pages/TransactionHistory";
 import Profile from "./pages/Profile";
 import MyClaims from "./pages/MyClaims";
 
-
-
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/landingPage" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/alimony-claim" element={<AlimonyClaim />} />
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Layout from "../components/Layout";
+import LandingPage from "../components/LandingPage/LandingPage";
+
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
+  useEffect(() => {
+    if (!isLoggedIn) navigate("/login");
+  }, [isLoggedIn, navigate]);
+
   return (
     <Layout isLandingPage>
-      <h1>Home</h1>
+      <LandingPage />
     </Layout>
   );
 }
